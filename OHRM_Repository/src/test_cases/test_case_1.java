5b8f4377534b7f1e0c896e7b943535d8534ba1e8
mirth <mirth@INBook_X1>
2022-07-07 08:36:24 +0530

    this is OHRM Project

---
+++ OHRM_Repository/src/test_cases/test_case_1.java
@@ -0,0 +1,10 @@
+package test_cases;
+
+public class test_case_1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("this is test case one");
+	}
+
+}
---
+++ OHRM_Repository/src/test_cases/test_case_2.java
@@ -0,0 +1,10 @@
+package test_cases;
+
+public class test_case_2 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("this is test case two");
+	}
+
+}
